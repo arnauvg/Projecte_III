1eb44220f36c3ae7135bace2e08ed5bed960ff2d
arnauvg <arnau.valls.i.gorchs@estudiantat.upc.edu>
2026-03-05 16:38:26 +0100

    mes buena i pick up mode

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public float pickDistance = 4f;
+    public float moveSpeed = 10f;
+
+    Rigidbody heldObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (heldObject == null)
+                TryPickUp();
+            else
+                Drop();
+        }
+
+        if (heldObject != null)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Plane plane = new Plane(Camera.main.transform.forward * -1,
+                                    Camera.main.transform.position + Camera.main.transform.forward * 1f);
+
+            if (plane.Raycast(ray, out float distance))
+            {
+                Vector3 targetPos = ray.GetPoint(distance);
+                Vector3 dir = targetPos - heldObject.position;
+                heldObject.linearVelocity = dir * moveSpeed;
+            }
+        }
+    }
+
+    void TryPickUp()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, pickDistance))
+        {
+            if (hit.rigidbody != null)
+            {
+                heldObject = hit.rigidbody;
+                heldObject.useGravity = false;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        heldObject.useGravity = true;
+        heldObject = null;
+    }
+}
