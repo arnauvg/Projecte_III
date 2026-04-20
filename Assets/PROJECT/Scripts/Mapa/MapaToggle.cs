90d5f168105bb2406f352aa68fbd89738121fd93
arnauvg <arnau.valls.i.gorchs@estudiantat.upc.edu>
2026-04-20 18:25:53 +0200

    mapa funcional al 50%

---
+++ Assets/PROJECT/Scripts/Mapa/MapaToggle.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapaToggle : MonoBehaviour
+{
+    public GameObject panelMapa;
+
+    private bool mapaAbierto = false;
+
+    void Start()
+    {
+        panelMapa.SetActive(false); // empieza cerrado
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (mapaAbierto)
+                CerrarMapa();
+            else
+                AbrirMapa();
+        }
+    }
+
+    void AbrirMapa()
+    {
+        panelMapa.SetActive(true);
+        mapaAbierto = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 0f; // pausa el juego
+    }
+
+    void CerrarMapa()
+    {
+        panelMapa.SetActive(false);
+        mapaAbierto = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1f; // reanuda el juego
+    }
+    public void IrAEscena(string nombreEscena)
+    {
+        Time.timeScale = 1f; // por si el juego estaba pausado
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
